f506f20ab0d6120ff6c588d4cea8d8b0e60ef829
Selin Karagullu <selin.karagullu@gmail.com>
2026-01-13 10:57:38 +0300

    style update

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -15,10 +15,25 @@ const Sidebar = () => {
 
                 <div className="recent">
                     <p className="recent-title">Recent</p>
-                    <div className="recent-entry"></div>
+                    <div className="recent-entry">
+                        <img src={assets.message_icon} alt="" />
+                        <p>What is react...</p>
+                    </div>
                 </div>
             </div>
             <div className="bottom">
+                <div className='bottom-item recent-entry'>
+                    <img src={assets.question_icon} alt="" />
+                    <p>Help</p>
+                </div>
+                <div className='bottom-item recent-entry'>
+                    <img src={assets.history_icon} alt="" />
+                    <p>Activity</p>
+                </div>
+                <div className='bottom-item recent-entry'>
+                    <img src={assets.setting_icon} alt="" />
+                    <p>Settings</p>
+                </div>
             </div>
         </div>
     )
