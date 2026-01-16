6725e8d7d3965a2a61fa8e48fe1f6ae3ea8e8f4d
Selin Karagullu <selin.karagullu@gmail.com>
2026-01-16 19:14:49 +0300

    Update practise.jsx

--- src/practise.jsx
+++ src/practise.jsx
@@ -1,1 +1,26 @@
 
+export default function Product ({title,price}) {
+return (
+    <div>
+        <h3>{title}</h3>
+        <p>{price}</p>
+    </div>
+)
+}
+
+const Button = ({children}) => {
+  return (
+    <button className="btn-primary">
+     {children}
+      
+    </button>
+  );
+};
+
+export default function App() {
+  return (
+    <Button>
+      Click Me <span>🚀</span>
+    </Button>
+  );
+}
