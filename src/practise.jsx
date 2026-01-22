46636e16d8e1781a8e6c75567ed807e85bbd9406
Selin Karagullu <selin.karagullu@gmail.com>
2026-01-22 19:01:21 +0300

    Update practise.jsx

--- src/practise.jsx
+++ src/practise.jsx
@@ -32,3 +32,12 @@ throw new Error ("")
 getData()
 
 },[])
+
+
+async function getTodo () {
+  const response = await fetch ("https://jsonplaceholder.typicode.com/todos/1")
+
+  const data = await response.json()
+}
+
+getTodo()
