776fed518cedf8295d6939818141e7d0670b9cb3
Selin Karagullu <selin.karagullu@gmail.com>
2026-02-03 17:11:31 +0300

    Update practise1.jsx

--- src/practise1.jsx
+++ src/practise1.jsx
@@ -1,14 +1,14 @@
+import { useEffect, useState, useContext, createContext } from "react"
 
 
+const MovieContext = createContext()
+export const useMovieContext = () => useContext (MovieContext)
 
-useEffect(()=>{
-const storedFavs = localStorage.getItem("favorites")
-
-if(storedFavs) setFavorites(JSON.parse(storedFavs))
-
-},[])
+export default function MovieProvider ({children}) {
+const [favorites,setFavorites] = useState([])
 
 
-useEffect(()=>{
-localStorage.setItem('favorites', JSON.stringify(favorites))
-},[favorites])
+    return <MovieContext.Provider>
+        {children}
+    </MovieContext.Provider>
+}
