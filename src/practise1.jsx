c12edd843b27de831d148eed0f623b2f4adb8b51
Selin Karagullu <selin.karagullu@gmail.com>
2026-02-05 22:30:37 +0300

    Update practise1.jsx

--- src/practise1.jsx
+++ src/practise1.jsx
@@ -1,8 +1,4 @@
-import { useEffect, useState, useContext, createContext } from "react"
 
-
-const MovieContext = createContext()
-export const useMovieContext = () => useContext (MovieContext)
 
 export default function MovieProvider ({children}) {
 const [favorites,setFavorites] = useState([])
