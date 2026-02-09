038f1aa8b510c574d97bdac67180eeedceb4115b
Selin Karagullu <selin.karagullu@gmail.com>
2026-02-09 21:20:21 +0300

    Update practise2.jsx

---
+++ src/practise2.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react"
+
+export default function RandomColor() {
+const [typeOfColor,setTypeOfColor] = useState('hex')
+const [color,setColor] = useState('#000000')
+function handleCreateRandomHexColor () {
+    const hex = 
+}
+function handleCreateRandomRgbColor () {}
+
+    return  <div style={{
+        width: "100vw",
+        height: '100vh',
+        background:color
+    }}>
+<button onClick={()=> setTypeOfColor('hex')}>Create Hex Color</button>
+<button onClick={()=> setTypeOfColor('hex')}>Create RGB Color</button>
+<button onClick={typeOfColor==="hex" ?handleCreateRandomHexColor : handleCreateRandomRgbColor}>Generate Random Color</button>
+    </div>
+}
