6e714f6e0da998a5d7c08c78b05c76b69a4142ed
Selin Karagullu <selin.karagullu@gmail.com>
2026-02-13 16:34:08 +0300

    Update practise2.jsx

--- src/practise2.jsx
+++ src/practise2.jsx
@@ -1,46 +1,31 @@
-import { useState } from "react"
+import { useEffect } from "react"
 
-export default function RandomColor() {
-    const [typeOfColor, setTypeOfColor] = useState('hex')
-    const [color, setColor] = useState('#000000')
+ export default function LoadMoreData () {
+    const[loading,setLoading] = useState(false)
+    const[products,setProducts] = useState([])
+    const[count,setCount] = useState(0)
 
-    function randomColorUtility(length) {
-        return Math.floor(Math.random() * length)
-    }
-    function handleCreateRandomHexColor() {
-        const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"]
-        let hexColor = "#"
+async function fetchProducts () {
+try {
+    setLoading(true)
+const response = await fetch ()
+const data = await response.json()
 
-        for (let i = 0; i < 6; i++) { hexColor += hex[randomColorUtility(hex.length)] }
-        setColor(hexColor)
-    }
-
-    function handleCreateRandomRgbColor() {
-        const r = randomColorUtility(256)
-        const g = randomColorUtility(256)
-        const b = randomColorUtility(256)
-        setColor(`rgb${r}, ${g},${b}`)
-    }
-
-    useEffect (()=>{
-typeOfColor==='rgb' ? handleCreateRandomRgbColor() : handleCreateRandomHexColor ()
-    }
-    ,[typeOfColor])
+if(data && data.products && data.products.length)
+setProducts(data.products)
+setLoading(false)
 
 
-    return <div style={{
-        width: "100vw",
-        height: '100vh',
-        background: color
-    }}>
-        <button onClick={() => setTypeOfColor('hex')}>Create Hex Color</button>
-        <button onClick={() => setTypeOfColor('hex')}>Create RGB Color</button>
-        <button onClick={typeOfColor === "hex" ? handleCreateRandomHexColor : handleCreateRandomRgbColor}>Generate Random Color</button>
-        <div style={{
-            display: "flex"
-        }}>
-            <h3>{typeOfColor === 'rgb' ? 'RGB color' : 'HEX Color'}</h3>
-            <h1>{color}</h1>
-        </div>
-    </div>
+
+}catch (e){
+setLoading(false)
+console.log(e)
+}
+}
+
+useEffect(()=>{
+    fetchProducts()
+},[])
+
+return
 }
