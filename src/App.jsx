ca5a4e95bc792ca47f455a12def4fef9c98d3c35
Selin Karagullu <selin.karagullu@gmail.com>
2026-03-24 21:51:30 +0300

    new project practise

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import {Route,Routes} from 'react-router-dom'
+import Favorites from "./Favorites"
+import Home from "./Home"
+import Details from "./Details"
 
 const App = () => {
   return (
-    <div>App</div>
+    <Routes>
+        <Route path="/favorites" element={<Favorites/>}/>
+        <Route path="/home" element ={<Home/>}/>
+        <Route path="/recipe/:id" element = {<Details/>}/>
+
+</Routes>
   )
 }
 
